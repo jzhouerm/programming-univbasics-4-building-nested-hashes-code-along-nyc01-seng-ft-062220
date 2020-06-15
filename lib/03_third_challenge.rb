a85d9527eaa0dfeb32f8d6c740562b4252ebb26b
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-15 01:08:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,25 +1,15 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
+      :patriarch => {:name => "Lord Montague", :age => "53"},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
    }
   }
